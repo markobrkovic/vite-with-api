23c7ca4afddfd1a63ef09daf27e226a82d1b1a6e
markobrkovic <querry999@gmail.com>
2021-11-22 11:37:42 +0100

    :green_heart: Add and fix storybook confgis

--- server/index.ts
+++ server/index.ts
@@ -1,13 +1,19 @@
 import express from 'express';
+import path from 'path';
 
-const port = 3005;
+const port = 3001;
 const app = express();
 
 app.get('/api/hello', (_request, response) => {
   response.json({ message: 'Hello from server' });
 });
 
-app.get('/', (_request, response) => response.send('Server is running'));
+// Serve production bundle
+app.use(express.static('dist'));
+
+app.get('*', (_request, response) =>
+  response.sendFile(path.join(__dirname, '../dist/index.html'))
+);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
